054b6730420533eb051e26e4cba57638d2826ec8
Banan Arab <banarabnana@gmail.com>
2025-08-21 03:03:14 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get("/name-banan", [TweetController::class, "index"])
