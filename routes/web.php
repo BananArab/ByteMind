cbffd3903f1b519df34cb5a00de96b691dc45de7
Banan Arab <banarabnana@gmail.com>
2025-08-28 21:08:45 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,34 @@ use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get("/name-banan", [TweetController::class, "index"])
 ->name('home');
 
-Route::post('/logout', LogoutController::class)
-    ->name('logout');
+Route::post("/logout",LogoutController::class )
+->name("logout");
 
-Route::get('/tweet/{tweet}', [TweetController::class, 'view'])
-    ->name('tweet.view');
-Route::post('/tweet/create', [TweetController::class, 'store'])
-    ->name('tweet.create');
+Route::get("/tweet/{tweet}", [TweetController::class, "view"])
+->name('tweet.view');
+Route::post("/tweet/create", [TweetController::class, "store"] )
+->name("tweet.create");
 
-Route::get('/login', [LoginController::class, 'create'])
-    ->name('login');
-Route::post('/login', [LoginController::class, 'store']);
 
-Route::get('/register', [RegisterController::class, 'create'])
-    ->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
+Route::get("/Login", [LoginController::class, "create"])
+->name('login');
+Route::post('/Login', [LoginController::class,'store']);
+
+Route::get("/Register", [RegisterController::class, "create"])
+->name('register');
+Route::post("/Register", [RegisterController::class, "store"]);
+
+Route::get("/test/{x}/{y}/{z}", function($x,$y,$z) {
+    return min($x, $y, $z);
+
+});
