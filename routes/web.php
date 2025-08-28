5a38f2661fc9f272695514469b9526746a3d9b5c
Banan Arab <banarabnana@gmail.com>
2025-08-28 22:37:05 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\RegisterController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/name-banan", [TweetController::class, "index"])
+Route::get("/", [TweetController::class, "index"])
 ->name('home');
 
 Route::post("/logout",LogoutController::class )
