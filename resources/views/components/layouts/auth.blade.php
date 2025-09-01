384d389a5271c3af47cf13cfad5e8c2db43d7cd5
Banan Arab <banarabnana@gmail.com>
2025-09-01 22:03:17 +0300

    Update auth.blade.php

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,14 +1,15 @@
 <x-layouts.default>
     <div class="flex flex-col justify-center items-center min-h-[100vh]">
         <div class="mb-12">
-            <h1 class="font-bold text-5xl text-white">
+            <h1 class="font-bold text-3xl sm:text-4xl md:text-5xl text-white">
                 ByteMind
-                <span class="icon-[tabler--code] size-12.5  text-white "></span>
+                <span class="icon-[tabler--code] size-8 sm:size-10 md:size-12 text-white"></span>
             </h1>
         </div>
-        <div class="mt-8">
+        <div class="w-full px-4 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mt-8">
             {{ $slot }}
         </div>
+
     </div>
 
 </x-layouts.default>
