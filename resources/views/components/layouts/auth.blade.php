68276354cbbca0339c69f74ed759360d7c7f9f2d
Banan Arab <banarabnana@gmail.com>
2025-09-01 23:04:19 +0300

    Update auth.blade.php

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,9 +1,9 @@
 <x-layouts.default>
     <div class="flex flex-col justify-center items-center min-h-[100vh]">
         <div class="mb-12">
-            <h1 class="font-bold text-4xl text-white">
+            <h1 class="font-bold text-[120px] text-white">
                 ByteMind
-                <span class="icon-[tabler--code] size-12.5 text-white"></span>
+                <span class="icon-[tabler--code] size-12 text-white"></span>
             </h1>
         </div>
         <div class="w-full px-4 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mt-8">
