0368a074d07674a0520495ad0e45c58c65dca986
Banan Arab <banarabnana@gmail.com>
2025-09-01 23:28:38 +0300

    Update auth.blade.php

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.default>
     <div class="flex flex-col justify-center items-center min-h-[100vh]">
         <div class="mb-12">
-            <h1 class="font-bold text-5xl text-white">
+            <h1 class="font-bold text-3.5xl text-white">
                 ByteMind
                 <span class="icon-[tabler--code] size-12 text-white"></span>
             </h1>
